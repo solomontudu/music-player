9b4f842cf231945178a4e0cadfa4a5ed06a5adb0
solomontudu <tukintudu@gmail.com>
2024-08-13 15:46:39 +0530

    added the functionality for updating the progress bar

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
 const music = document.querySelector("audio");
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
 const playBtn = document.getElementById("play");
+const progressContainer = document.getElementById("progress-container");
+const progress = document.getElementById("progress");
+const duration = document.getElementById("duration");
+const currentTime = document.getElementById("current-time");
+// music
+const songs = [
+  {
+    name: "jacinto-1",
+    displayName: "Electric Chill Machine",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-2",
+    displayName: "Seven Nation Army (Remix)",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-3",
+    displayName: "Good night, Disco Queen",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "metric-1",
+    displayName: "Front Row",
+    artist: "Jacinto Design",
+  },
+];
 
 // check if playing
 let isPlaying = false;
 
 // play
 function playSong() {
-  let isPlaying = false;
+  isPlaying = true;
   playBtn.classList.replace("fa-play", "fa-pause");
   playBtn.setAttribute("title", "Pause");
   music.play();
@@ -26,3 +56,54 @@ function pauseSong() {
 playBtn.addEventListener("click", () => {
   isPlaying ? pauseSong() : playSong();
 });
+
+// update DOM
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+}
+
+// current song
+let songIndex = 0;
+
+// previous song
+function prevSong() {
+  if (songIndex > 0) {
+    songIndex--;
+  } else {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// next song
+function nextSong() {
+  if (songIndex < songs.length - 1) {
+    songIndex++;
+  } else {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// onload - select first song
+loadSong(songs[songIndex]);
+
+// update progress bar & time
+function updateProgressBar(e) {
+  if (isPlaying) {
+    const { duration, currentTime } = e.srcElement;
+    // update progress bar
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+  }
+}
+
+// event listeners
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+music.addEventListener("timeupdate", updateProgressBar);
